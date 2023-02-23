74f2245d3d1634646813e7505a58c6b005cc1b10
Daniel Hodvogner <dhodvogner@gmail.com>
2023-02-24 00:13:47 +0100

    feat: Esy-state added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.use(express.static('public'));
+app.use('/esy-state',  express.static('node_modules/@esyfyi/esy-state', { index: ["index.mjs"] }));
 
 app.listen(port, () => {
   console.log(`Esy-State website is listening on port ${port}`);
